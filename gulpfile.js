aba8532417b7081f5c3819849a62ed3f9863091c
Tyler Graf <tylergraf@gmail.com>
2016-11-12 21:52:14 -0700

    cool.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('start', function () {
 gulp.task('generate-service-worker', function(callback) {
   var path = require('path');
   var swPrecache = require('sw-precache');
-  var rootDir = 'public';
+  var rootDir = 'src';
 
   swPrecache.write(path.join(rootDir, 'service-worker.js'), {
     staticFileGlobs: [rootDir + '/elements/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
